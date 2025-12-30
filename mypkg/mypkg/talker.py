06fcd19bc91842c29d71d499d4376780a8403f32
Rui <s24c1081ef@s.chibakoudai.jp>
2025-12-31 05:23:54 +0900

    Add mypkg and person_msgs packages

---
+++ mypkg/mypkg/talker.py
@@ -0,0 +1,17 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.msg import Person #変更
+rclpy.init()
+node = Node("talker")
+pub = node.create_publisher(Person, "person", 10) #変更
+n = 0
+def cb():
+    global n
+    msg = Person()         #送信するデータの型を変更
+    msg.name = "上田隆一"  #msgファイルに書いた「name」
+    msg.age = n            #msgファイルに書いた「age」                             
+    pub.publish(msg)
+    n += 1
+def main():
+    node.create_timer(0.5, cb)
+    rclpy.spin(node)
