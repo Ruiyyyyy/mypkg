bae736f4d305e48a64bbe8abac9246a4a8b28b54
Rui <s24c1081ef@s.chibakoudai.jp>
2025-12-31 18:35:59 +0900

    Manual fix: removed bash commands from python files

--- mypkg/mypkg/talker.py
+++ mypkg/mypkg/talker.py
@@ -1,13 +1,10 @@
-cat << 'EOF' > ~/ros2_ws/src/mypkg/mypkg/talker.py
 import random
 
-import rclpy
 from person_msgs.msg import Person
+import rclpy
 from rclpy.node import Node
 
-
 class SensorTalker(Node):
-
     def __init__(self):
         super().__init__('sensor_talker')
         self.pub = self.create_publisher(Person, 'sensor_data', 10)
@@ -18,14 +15,11 @@ class SensorTalker(Node):
         locations = ['玄関', 'リビング', 'ベランダ', '窓']
         msg.name = random.choice(locations)
         msg.age = random.randint(0, 100)
-
         self.pub.publish(msg)
         self.get_logger().info(f'監視中: {msg.name} でレベル {msg.age} を検知')
-
 
 def main():
     rclpy.init()
     node = SensorTalker()
     rclpy.spin(node)
     rclpy.shutdown()
-EOF
