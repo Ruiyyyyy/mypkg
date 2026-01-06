05e5ef3baf9c4c7d5ae77fc9133701ba57a0f4ac
Rui <s24c1081ef@s.chibakoudai.jp>
2026-01-06 20:28:10 +0900

    listener.pyの編集

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,26 +1,20 @@
-listener.py
-
-
-from person_msgs.msg import Person
 import rclpy
 from rclpy.node import Node
-
+from std_msgs.msg import Int16
 
 class AlertListener(Node):
-
     def __init__(self):
         super().__init__('alert_listener')
-        self.sub = self.create_subscription(Person, 'sensor_data', self.cb, 10)
+        self.sub = self.create_subscription(Int16, 'sensor_data', self.cb, 10)
 
     def cb(self, msg):
         status = '異常なし'
-        if msg.age > 80:
+        if msg.data > 80:
             status = '警告！'
-        elif msg.age > 40:
+        elif msg.data > 40:
             status = '注意'
 
-        self.get_logger().info(f'通知: [{msg.name}] 状態: {status} ({msg.age})')
-
+        self.get_logger().info(f'通知: 状態: {status} ({msg.data})')
 
 def main():
     rclpy.init()
