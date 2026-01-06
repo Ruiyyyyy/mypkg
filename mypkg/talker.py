6eb73a275ac3b4d677b130a933b60ee7b79bc18a
Rui <s24c1081ef@s.chibakoudai.jp>
2026-01-06 19:33:26 +0900

    talker.py の修正

---
+++ mypkg/talker.py
@@ -0,0 +1,27 @@
+talker.py
+
+import random
+
+from person_msgs.msg import Person
+import rclpy
+from rclpy.node import Node
+
+class SensorTalker(Node):
+    def __init__(self):
+        super().__init__('sensor_talker')
+        self.pub = self.create_publisher(Person, 'sensor_data', 10)
+        self.timer = self.create_timer(2.0, self.timer_callback)
+
+    def timer_callback(self):
+        msg = Person()
+        locations = ['玄関', 'リビング', 'ベランダ', '窓']
+        msg.name = random.choice(locations)
+        msg.age = random.randint(0, 100)
+        self.pub.publish(msg)
+        self.get_logger().info(f'監視中: {msg.name} でレベル {msg.age} を検知')
+
+def main():
+    rclpy.init()
+    node = SensorTalker()
+    rclpy.spin(node)
+    rclpy.shutdown()
