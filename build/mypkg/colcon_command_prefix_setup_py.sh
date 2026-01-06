b956e8b31db68a40a57feea3e4a4ab06af19e220
Rui <s24c1081ef@s.chibakoudai.jp>
2026-01-06 17:29:48 +0900

    不要なファイルを削除

--- build/mypkg/colcon_command_prefix_setup_py.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/rui810/ros2_ws/src/install/person_msgs/share/person_msgs/package.sh"
-. "/home/rui810/ros2_ws/src/install/mypkg/share/mypkg/package.sh"
