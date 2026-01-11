87ad86086b66a32f0e50115ab01324eac3c93180
Rui <s24c1081ef@s.chibakoudai.jp>
2026-01-11 18:11:32 +0900

    listener.py の編集

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -5,11 +5,13 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
+from std_msgs.msg import Int16, String
 
 class AlertListener(Node):
     def __init__(self):
         super().__init__('alert_listener')
         self.sub = self.create_subscription(Int16, 'sensor_data', self.cb, 10)
+        self.pub = self.create_publisher(String, 'alert_status', 10)
 
     def cb(self, msg):
         status = 'Normal'
@@ -19,6 +21,10 @@ class AlertListener(Node):
             status = 'Warning'
         
         self.get_logger().info(f'Listen: {status} ({msg.data})')
+
+        alert_msg = String()
+        alert_msg.data = status
+        self.pub.publish(alert_msg)
 
 def main():
     rclpy.init()
